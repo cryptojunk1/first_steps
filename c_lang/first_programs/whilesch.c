5c4bbfbb917ef64bf6fd3fac1b60fbd182a95c7b
René Messner <remes@LAPTOP-3QJU7RD3.localdomain>
2023-04-23 18:30:09 +0200

    made a while in c

--- c_lang/first_programs/whilesch.c
+++ c_lang/first_programs/whilesch.c
@@ -4,8 +4,12 @@ int     main()  {
             int a = 5;
             int b = 1;
 			
-			while(a > 0)	{
-					printf("a ist %d\n" , a);
+			while(a >= 0)	{   //kann auch mit && || kombiniert werden
+					while(b < 3)	{
+							printf("b ist zu klein\n");
+							b++;
+					}
+					printf("A = %d\n", a);
 					a--;
 			}      
 	      return 0;
